6e56584fac7a7e1741aabf82c008270820ff2494
Bo Price <boprice2012@gmail.com>
2024-07-31 13:47:01 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = 1049732316240723968; // your league ID
+export const leagueID = 1049732316240723968n; // your league ID
 export const leagueName = "Huddle Buddies"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
