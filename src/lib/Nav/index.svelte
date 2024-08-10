75aecb53aa9dbe130e8ea5be9a12759703a0d5d1
Bo Price <boprice2012@gmail.com>
2024-08-10 19:34:26 -0400

    Change tab name

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -29,7 +29,7 @@
 </script>
 
 <svelte:head>
-	<title>{!$page.url.pathname[1] ? 'Home' : $page.url.pathname[1].toUpperCase() + $page.url.pathname.slice(2)} | League Page</title>
+	<title>{!$page.url.pathname[1] ? 'Home' : $page.url.pathname[1].toUpperCase() + $page.url.pathname.slice(2)} | RYE</title>
 </svelte:head>
 
 <style>
